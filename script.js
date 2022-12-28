acfa0e59a2e333396c9ebbff0f9abc64c2abdff5
Quvonchbek048 <quvonchbeku048@gmail.com>
2022-12-28 22:16:27 +0500

    Team Quvonchbek

--- script.js
+++ script.js
@@ -1,55 +1,32 @@
-// let a = +prompt("Birinchi sonni kiriting")
-// let b = +prompt("Ikkinchi sonni kiriting")
-// let c = prompt("Amalni tanlang + - * / %")
-
-// if (c == "qo'sh" || c == "+") {
-//     alert(a + b)
-//     console.log(a + b);
-// } 
-// else if (c == "ayir" || c == "-") {
-//     alert(a - b)
-//     console.log(a - b);
-// }
-// else if (c == "ko'paytir" || c == "*") {
-//     alert(a * b)
-//     console.log(a * b);
-// }
-// else if (c == "bo'l" || c == "/") {
-//     alert(a / b)
-//     console.log(a / b);
-// }
-// else if (c == "q-li bo'l" || c == "%") {
-//     alert(a % b)
-//     console.log(a % b);
-// }
-
-
-// let x = prompt("Raqamni kiriting")
-
-// if (x % 2 == 0) {
-//     console.log("bu raqam juft");
-//     alert("bu raqam juft")
-// }
-// else {
-//     console.log("bu raqam toq");
-//     alert("bu raqam toq")
-// }
-
-
-// ------------------------------------------------------------------------------------------------------------------------- //
-
-
 function dclick() {
     document.querySelector('body').style.backgroundColor = "#000";
-    document.getElementById('dark').style.backgroundColor = "#fff";
-    document.getElementById('dark').style.color = "#000";
-    document.getElementById('light').style.backgroundColor = "#fff";
-    document.getElementById('light').style.color = "#000";
+    document.querySelector('body').style.transition = "1.7s";
 }
 function lclick() {
     document.querySelector('body').style.backgroundColor = "#fff";
-    document.getElementById('dark').style.backgroundColor = "#000";
-    document.getElementById('dark').style.color = "#fff";
-    document.getElementById('light').style.backgroundColor = "#000";
-    document.getElementById('light').style.color = "#fff";
+    document.querySelector('body').style.transition = "1.7s";
+}
+function iclick() {
+    document.querySelector('body').style.backgroundColor = "#0dcaf0";
+    document.querySelector('body').style.transition = "1.7s";
+}
+function sclick() {
+    document.querySelector('body').style.backgroundColor = "#198754";
+    document.querySelector('body').style.transition = "1.7s";
+}
+function dnclick() {
+    document.querySelector('body').style.backgroundColor = "#dc3545";
+    document.querySelector('body').style.transition = "1.7s";
+}
+function wclick() {
+    document.querySelector('body').style.backgroundColor = "#ffc107";
+    document.querySelector('body').style.transition = "1.7s";
+}
+function seclick() {
+    document.querySelector('body').style.backgroundColor = "#6c757d";
+    document.querySelector('body').style.transition = "1.7s";
+}
+function defclick() {
+    document.querySelector('body').style.backgroundColor = "rgb(63, 90, 90)";
+    document.querySelector('body').style.transition = ".1s";
 }
