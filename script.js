889ccc774fc0cb7188cef02e459650d23b13fb71
Quvonchbek048 <quvonchbeku048@gmail.com>
2023-02-08 20:06:29 +0500

    Team Quvonchbek

--- script.js
+++ script.js
@@ -2,91 +2,91 @@ function dclick() {
     document.querySelector('body').style.backgroundColor = "#000";
     document.querySelector('body').style.transition = "1.7s";
     document.getElementById('dark').style.border = "none";
-    document.getElementById('light').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('info').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('success').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('danger').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('warning').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('light').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('info').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('success').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('danger').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('warning').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 3.5px solid";
     document.getElementById('default').style.display = "block";
-    document.getElementById('default').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('default').style.border = "rgb(12, 0, 255) 3.5px solid";
 }
 function lclick() {
     document.querySelector('body').style.backgroundColor = "#fff";
     document.querySelector('body').style.transition = "1.7s";
     document.getElementById('light').style.border = "none";
-    document.getElementById('dark').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('info').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('success').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('danger').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('warning').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('dark').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('info').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('success').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('danger').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('warning').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 3.5px solid";
     document.getElementById('default').style.display = "block";
-    document.getElementById('default').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('default').style.border = "rgb(12, 0, 255) 3.5px solid";
 }
 function iclick() {
     document.querySelector('body').style.backgroundColor = "#0dcaf0";
     document.querySelector('body').style.transition = "1.7s";
     document.getElementById('info').style.border = "none";
-    document.getElementById('light').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('dark').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('success').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('danger').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('warning').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('light').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('dark').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('success').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('danger').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('warning').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 3.5px solid";
     document.getElementById('default').style.display = "block";
-    document.getElementById('default').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('default').style.border = "rgb(12, 0, 255) 3.5px solid";
 }
 function sclick() {
     document.querySelector('body').style.backgroundColor = "#198754";
     document.querySelector('body').style.transition = "1.7s";
     document.getElementById('success').style.border = "none";
-    document.getElementById('light').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('info').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('dark').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('danger').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('warning').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('light').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('info').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('dark').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('danger').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('warning').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 3.5px solid";
     document.getElementById('default').style.display = "block";
-    document.getElementById('default').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('default').style.border = "rgb(12, 0, 255) 3.5px solid";
 }
 function dnclick() {
     document.querySelector('body').style.backgroundColor = "#dc3545";
     document.querySelector('body').style.transition = "1.7s";
     document.getElementById('danger').style.border = "none";
-    document.getElementById('light').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('info').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('success').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('dark').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('warning').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('light').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('info').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('success').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('dark').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('warning').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 3.5px solid";
     document.getElementById('default').style.display = "block";
-    document.getElementById('default').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('default').style.border = "rgb(12, 0, 255) 3.5px solid";
 }
 function wclick() {
     document.querySelector('body').style.backgroundColor = "#ffc107";
     document.querySelector('body').style.transition = "1.7s";
     document.getElementById('warning').style.border = "none";
-    document.getElementById('light').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('info').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('success').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('danger').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('dark').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('light').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('info').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('success').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('danger').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('dark').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 3.5px solid";
     document.getElementById('default').style.display = "block";
-    document.getElementById('default').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('default').style.border = "rgb(12, 0, 255) 3.5px solid";
 }
 function seclick() {
     document.querySelector('body').style.backgroundColor = "#6c757d";
     document.querySelector('body').style.transition = "1.7s";
     document.getElementById('secondary').style.border = "none";
-    document.getElementById('light').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('info').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('success').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('danger').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('warning').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('dark').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('default').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('light').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('info').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('success').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('danger').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('warning').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('dark').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('default').style.border = "rgb(12, 0, 255) 3.5px solid";
     document.getElementById('default').style.display = "block";
 }
 function defclick() {
@@ -94,11 +94,11 @@ function defclick() {
     document.querySelector('body').style.transition = ".1s";
     document.getElementById('default').style.border = "none";
     document.getElementById('default').style.display = "none";
-    document.getElementById('light').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('info').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('success').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('danger').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('warning').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 1.5px solid";
-    document.getElementById('dark').style.border = "rgb(12, 0, 255) 1.5px solid";
+    document.getElementById('light').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('info').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('success').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('danger').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('warning').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('secondary').style.border = "rgb(12, 0, 255) 3.5px solid";
+    document.getElementById('dark').style.border = "rgb(12, 0, 255) 3.5px solid";
 }
